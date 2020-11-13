e581b524c270ab419dd8314dc3fdb2c05a43be0a
mledoxvii <marianoxvii@gmail.com>
2020-11-13 12:24:02 +0100

    fix-distribution: fix: Update framework zip checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: "SInject",
             url: "https://github.com/mledoxvii/SInject/releases/download/1.0.0/SInject.xcframework.zip",
-            checksum: "36f412d6de783e07839d021cea083d4a114dc36a45cb62dfb998f26564745989"
+            checksum: "bca32843f30bfa9614659752fe995f3bd275c993f9bc211b193323028241820a"
         )
     ]
 )
